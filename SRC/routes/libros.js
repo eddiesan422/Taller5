a5d56b4376c9fb36f6e0d2af481f8e727f256cb5
eddiesan118 <eddies.hernandezm@konradlorenz.edu.co>
2023-04-18 22:37:39 -0500

    Peticiones http
    Se completa el backend con todas las peticiones http

--- SRC/routes/libros.js
+++ SRC/routes/libros.js
@@ -9,4 +9,60 @@ router.post("/libros", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
-module.exports = router;
+//endpoint para Consultar todos los animales
+router.get("/libros", (req, res) => {
+    libroSchema
+      .find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  });
+  
+  //endopoint para Consultar un animal
+  router.get("/libros/:id", (req, res) => {
+    const { id } = req.params;
+    libroSchema
+      .findOne({ _id: id })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  });
+  
+  //endpoint para Modificar un animal usando el id
+  router.put("/libros/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo } = req.body;
+    libroSchema
+      .updateOne(
+        { _id: id },
+        {
+          $set: { nombre, edad, tipo },
+        }
+      )
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  });
+  
+  //endpoint para Eliminar un animal usando el id
+  router.delete("/libros/:id", (req, res) => {
+    const { id } = req.params;
+    libroSchema
+      .findByIdAndDelete({ _id: id })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((error) => {
+        res.json({ message: error });
+      });
+  });
+  module.exports = router;
